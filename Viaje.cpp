c3f3185e97893f01a981742a1151c4f8b53f2ad1
Erick-Hilario <jcuentafor@gmail.com>
2025-05-08 09:48:00 -0600

    Add files via upload

---
+++ Viaje.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  //Realizar una compra.
+
+   int a;
+
+   cout<<"Tienes la oportunidad de viajar, a donde irias?"<<endl;
+   cout<<"1. Ir a Alaska"<<endl;
+   cout<<"2. Ir a Brasil"<<endl;
+   cout<<"3. Ir a Inglaterra"<<endl;
+   cout<<"4. Ir a Puerto Rico"<<endl;
+   cout<<"5. Ir a Dubai"<<endl;
+   cin>>a;
+
+
+
+
+
+    return 0;
+}
